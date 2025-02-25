337b5aa693565c245cf10016bf1a06d565d98b82
venku.buragadda <venku.buragadda.acc@gmail.com>
2025-02-25 11:42:03 -0600

    Clean-up of comments.

--- src/connection/typesense_connection.py
+++ src/connection/typesense_connection.py
@@ -2,9 +2,9 @@ import typesense
 
 client: typesense.Client = typesense.Client({
     'nodes':[{
-    'host': 'localhost',  # For Typesense Cloud use xxx.a1.typesense.net
-    'port': '8108',       # For Typesense Cloud use 443
-    'protocol': 'http'    # For Typesense Cloud use https
+    'host': 'localhost',  
+    'port': '8108',       
+    'protocol': 'http'
     }
         
     ],
